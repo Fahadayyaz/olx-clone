13771461edba5628a159b652e9d52da18a64b647
Fahad Ayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2026-03-12 23:50:03 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PostAd from "./pages/PostAd";
+import AdDetail from "./pages/AdDetail";
+import MyAds from "./pages/MyAds";
+import Profile from "./pages/Profile";
+import ChatPage from "./pages/ChatPage";
+import CategoryPage from "./pages/CategoryPage";
+import SearchResults from "./pages/SearchResults";
+import FeaturedCheckout from "./pages/FeaturedCheckout";
+import GoogleCallback from "./pages/GoogleCallback";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/ad/:id" element={<AdDetail />} />
+            <Route path="/category/:slug" element={<CategoryPage />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/auth/google/callback" element={<GoogleCallback />} />
+            <Route
+              path="/post-ad"
+              element={
+                <ProtectedRoute>
+                  <PostAd />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/my-ads"
+              element={
+                <ProtectedRoute>
+                  <MyAds />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/chats"
+              element={
+                <ProtectedRoute>
+                  <ChatPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/featured/:adId"
+              element={
+                <ProtectedRoute>
+                  <FeaturedCheckout />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </main>
+        <Footer />
+        <ToastContainer position="bottom-right" autoClose={3000} />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
